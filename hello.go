f7cda51df924a3c4d008757e4864b2f516265288
issei.terada <issei.terada@showcase-gig.com>
2022-05-06 02:08:19 +0900

    Initial commit

---
+++ hello.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+const spanish = "Spanish"
+const french = "French"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	prefix := greeetingPrefix(language)
+
+	return prefix + name
+}
+
+func greeetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
---
+++ pointers-errors/wallet.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+//一貫性を持たせるためにBalanceのレシーバーもポインタに
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
